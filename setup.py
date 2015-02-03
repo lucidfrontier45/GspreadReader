3564c41f8a7a2ef900e23f42a5ef6a72b6998fc4
Shiqiao Du <s.du@freebit.net>
2015-02-03 17:42:02 +0900

    added setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='GspreadReader',
+    version='0.1',
+    packages=['gspread_reader'],
+    url='',
+    license='MIT',
+    author='Shiqiao Du',
+    author_email='lucidfrontier.45@gmail.com',
+    description='CSV like Google spreadsheet reader'
+)
